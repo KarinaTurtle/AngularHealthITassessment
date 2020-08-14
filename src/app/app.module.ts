d6fab193cd008fb554bfc83e9cd3d789c0cf1a1f
karinaTurtle <karina.chavarin28@gmail.com>
2020-08-14 12:53:42 -0500

    environment setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SelectStateComponent } from './components/select-state/select-state.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { StaticInfoComponent } from './components/static-info/static-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectStateComponent,
+    ChartComponent,
+    StaticInfoComponent
   ],
   imports: [
     BrowserModule
