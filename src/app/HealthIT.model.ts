96458af480f2f5fc9fe49edd28722b87a2b5de5a
karinaTurtle <karina.chavarin28@gmail.com>
2020-08-16 12:06:55 -0500

    2015 dropdown

--- src/app/HealthIT.model.ts
+++ src/app/HealthIT.model.ts
@@ -1,3 +1,8 @@
 export class User {
   region: string;
+  region_code: string;
+  period: string;
+  atleast_one_measure: string;
+  immunization_measure: string;
+  reportable_lab_results_measure: string;
 }
